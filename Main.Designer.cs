c74c220e96a9b92493e40344f69892d1ef06b28e
Mauro Leggieri <mxmauro@mauroleggieri.com.ar>
2017-01-21 16:44:53 -0500

    Initial commit.

---
+++ Main.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SecurityDescriptorConverter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSource = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.lblDest = new System.Windows.Forms.Label();
+            this.btnBinSd2StrSd = new System.Windows.Forms.Button();
+            this.btnStrSd2BinSd = new System.Windows.Forms.Button();
+            this.btnStrSid2BinSid = new System.Windows.Forms.Button();
+            this.btnBinSid2StrSid = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(12, 9);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(62, 13);
+            this.lblSource.TabIndex = 0;
+            this.lblSource.Text = "To convert:";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(15, 25);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(574, 138);
+            this.txtSource.TabIndex = 1;
+            // 
+            // txtDest
+            // 
+            this.txtDest.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDest.Location = new System.Drawing.Point(15, 194);
+            this.txtDest.Multiline = true;
+            this.txtDest.Name = "txtDest";
+            this.txtDest.ReadOnly = true;
+            this.txtDest.Size = new System.Drawing.Size(574, 138);
+            this.txtDest.TabIndex = 3;
+            // 
+            // lblDest
+            // 
+            this.lblDest.AutoSize = true;
+            this.lblDest.Location = new System.Drawing.Point(12, 178);
+            this.lblDest.Name = "lblDest";
+            this.lblDest.Size = new System.Drawing.Size(59, 13);
+            this.lblDest.TabIndex = 2;
+            this.lblDest.Text = "Converted:";
+            // 
+            // btnBinSd2StrSd
+            // 
+            this.btnBinSd2StrSd.Location = new System.Drawing.Point(15, 353);
+            this.btnBinSd2StrSd.Name = "btnBinSd2StrSd";
+            this.btnBinSd2StrSd.Size = new System.Drawing.Size(139, 35);
+            this.btnBinSd2StrSd.TabIndex = 4;
+            this.btnBinSd2StrSd.Text = "Binary SD to String SD";
+            this.btnBinSd2StrSd.UseVisualStyleBackColor = true;
+            this.btnBinSd2StrSd.Click += new System.EventHandler(this.btnBinSd2StrSd_Click);
+            // 
+            // btnStrSd2BinSd
+            // 
+            this.btnStrSd2BinSd.Location = new System.Drawing.Point(160, 353);
+            this.btnStrSd2BinSd.Name = "btnStrSd2BinSd";
+            this.btnStrSd2BinSd.Size = new System.Drawing.Size(139, 35);
+            this.btnStrSd2BinSd.TabIndex = 5;
+            this.btnStrSd2BinSd.Text = "String SD to Binary SD";
+            this.btnStrSd2BinSd.UseVisualStyleBackColor = true;
+            this.btnStrSd2BinSd.Click += new System.EventHandler(this.btnStrSd2BinSd_Click);
+            // 
+            // btnStrSid2BinSid
+            // 
+            this.btnStrSid2BinSid.Location = new System.Drawing.Point(450, 353);
+            this.btnStrSid2BinSid.Name = "btnStrSid2BinSid";
+            this.btnStrSid2BinSid.Size = new System.Drawing.Size(139, 35);
+            this.btnStrSid2BinSid.TabIndex = 7;
+            this.btnStrSid2BinSid.Text = "String Sid to Binary Sid";
+            this.btnStrSid2BinSid.UseVisualStyleBackColor = true;
+            this.btnStrSid2BinSid.Click += new System.EventHandler(this.btnStrSid2BinSid_Click);
+            // 
+            // btnBinSid2StrSid
+            // 
+            this.btnBinSid2StrSid.Location = new System.Drawing.Point(305, 353);
+            this.btnBinSid2StrSid.Name = "btnBinSid2StrSid";
+            this.btnBinSid2StrSid.Size = new System.Drawing.Size(139, 35);
+            this.btnBinSid2StrSid.TabIndex = 6;
+            this.btnBinSid2StrSid.Text = "Binary Sid to String Sid";
+            this.btnBinSid2StrSid.UseVisualStyleBackColor = true;
+            this.btnBinSid2StrSid.Click += new System.EventHandler(this.btnBinSid2StrSid_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(495, 404);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(94, 23);
+            this.btnQuit.TabIndex = 8;
+            this.btnQuit.Text = "&Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 442);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnStrSid2BinSid);
+            this.Controls.Add(this.btnBinSid2StrSid);
+            this.Controls.Add(this.btnStrSd2BinSd);
+            this.Controls.Add(this.btnBinSd2StrSd);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.lblDest);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.lblSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Security Descriptor Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label lblDest;
+        private System.Windows.Forms.Button btnBinSd2StrSd;
+        private System.Windows.Forms.Button btnStrSd2BinSd;
+        private System.Windows.Forms.Button btnStrSid2BinSid;
+        private System.Windows.Forms.Button btnBinSid2StrSid;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace SecurityDescriptorConverter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBinSd2StrSd_Click(object sender, EventArgs e)
+        {
+            string s;
+            byte[] aSrc;
+            RawSecurityDescriptor sd;
+
+            s = txtSource.Text.Trim();
+            if (s.Length == 0)
+            {
+                MessageBox.Show("Please fill the source binary SDDL", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            aSrc = ByteArrayFromString(s);
+            if (aSrc == null)
+            {
+                MessageBox.Show("Invalid binary SDDL", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            try
+            {
+                sd = new RawSecurityDescriptor(aSrc, 0);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid binary SDDL", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            txtDest.Text = sd.GetSddlForm(AccessControlSections.All);
+        }
+
+        private void btnStrSd2BinSd_Click(object sender, EventArgs e)
+        {
+            RawSecurityDescriptor sd;
+            string s;
+            byte[] aDest;
+
+            s = txtSource.Text.Trim();
+            if (s.Length == 0)
+            {
+                MessageBox.Show("Please fill the source SDDL string", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            try
+            {
+                sd = new RawSecurityDescriptor(s);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid SDDL string", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            aDest = new byte[sd.BinaryLength];
+            sd.GetBinaryForm(aDest, 0);
+            txtDest.Text = BitConverter.ToString(aDest).Replace("-", " ");
+        }
+
+        private void btnBinSid2StrSid_Click(object sender, EventArgs e)
+        {
+            string s;
+            byte[] aSrc;
+            SecurityIdentifier sid;
+
+            s = txtSource.Text.Trim();
+            if (s.Length == 0)
+            {
+                MessageBox.Show("Please fill the binary SDDL", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            aSrc = ByteArrayFromString(s);
+            if (aSrc == null)
+            {
+                MessageBox.Show("Invalid binary SDDL", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            try
+            {
+                sid = new SecurityIdentifier(aSrc, 0);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid binary SDDL", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            txtDest.Text = sid.ToString();
+        }
+
+        private void btnStrSid2BinSid_Click(object sender, EventArgs e)
+        {
+            SecurityIdentifier sid;
+            string s;
+            byte[] aDest;
+
+            s = txtSource.Text.Trim();
+            if (s.Length == 0)
+            {
+                MessageBox.Show("Please fill the source SDDL string", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            try
+            {
+                sid = new SecurityIdentifier(s);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid SDDL string", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSource.Focus();
+                return;
+            }
+            aDest = new byte[sid.BinaryLength];
+            sid.GetBinaryForm(aDest, 0);
+            txtDest.Text = BitConverter.ToString(aDest).Replace("-", " ");
+        }
+
+        private byte[] ByteArrayFromString(string s)
+        {
+            byte[] aData;
+            int i, charsCount;
+
+            for (i = charsCount = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ' || s[i] == '\t' || s[i] == '\r' || s[i] == '\n' || s[i] == ',' || s[i] == '\\')
+                    continue;
+                if (HexCharToDigit(s[i]) < 0)
+                    return null;
+                if (i+1 >= s.Length)
+                    return null;
+                if (HexCharToDigit(s[i+1]) < 0)
+                    return null;
+                i++;
+                charsCount++;
+            }
+            aData = new byte[charsCount];
+            for (i = charsCount = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ' || s[i] == '\t' || s[i] == '\r' || s[i] == '\n' || s[i] == ',' || s[i] == '\\')
+                    continue;
+                aData[charsCount] = (byte)(HexCharToDigit(s[i]) * 16 + HexCharToDigit(s[i + 1]));
+                i++;
+                charsCount++;
+            }
+            return aData;
+        }
+
+        private int HexCharToDigit(char ch)
+        {
+            if (ch >= '0' && ch <= '9')
+                return (int)ch - 48;
+            if (ch >= 'A' && ch <= 'F')
+                return (int)ch - 55;
+            if (ch >= 'a' && ch <= 'f')
+                return (int)ch - 87;
+            return -1;
+        }
+    }
+}
